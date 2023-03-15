d14af62f6887c9752750dcecc79328955c7bd46b
AlexaBabicheff <126571318+AlexaBabicheff@users.noreply.github.com>
2023-03-15 23:17:57 +0100

    Add files via upload
    Home work to lesson DD.14.03.2023

---
+++ StringExamples.java
@@ -0,0 +1,11 @@
+package com.homeworks;
+
+public class StringExamples {
+    public static void main(String[] args) {
+        String a = "Hello, Java! It's a JavaBasic course";
+        System.out.println(a.length());
+        System.out.println(a.charAt(11));
+        System.out.println(a.substring(13,24));
+        System.out.println(a.substring(20));
+    }
+}
